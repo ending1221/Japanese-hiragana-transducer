6e54495c481ef67836858a304d309017e43b7725
Yau <ending1221@gmail.com>
2020-09-11 17:34:22 +0800

    新增css基本樣式及按鈕功能
    按鈕: 平假名&片假名轉換

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const Hiragana = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'を', 'ん'];
+const Katakana = ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', 'ヲ', 'ン'];
+const Romanization = ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'shi', 'su', 'se', 'so', 'ta', 'chi', 'tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho', 'ma', 'mi', 'mu', 'me', 'mo', 'ya', 'yu', 'yo', 'ra', 'ri', 'ru', 're', 'ro', 'wa', 'wo', 'n']
+
+
+const $panels = document.querySelector('.panels');
+const $list = $panels.querySelector('.translist');
+const $convertBtn = document.querySelector('.buttons #convert');
+let State = true;
+
+const getTransList = function() {  
+    const state = State ? Hiragana : Katakana;
+    $list.innerHTML = '';
+    for (let i = 0; i < Hiragana.length; i++) {
+        let node = document.createElement("li");
+        let textnode = document.createTextNode(state[i]+' / '+Romanization[i]);
+        node.appendChild(textnode);
+        $list.appendChild(node);
+    }
+}
+
+const convertList = function() {  
+    
+    $convertBtn.addEventListener('click', function() {  
+        State = !State;
+        let text = State ? 'あ' : 'ア';
+        $convertBtn.innerHTML = text;
+        getTransList();
+    })
+}
+
+convertList();
+getTransList();
+
+
+// console.log($list);
